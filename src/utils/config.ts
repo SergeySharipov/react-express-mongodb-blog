e7a9f0d36142602d219ae19545b96ca32a208573
SergeySharipov <sharipov14@gmail.com>
2022-04-15 11:56:32 -0400

    Fix backend config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 const PORT = process.env.PORT
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@todo.yalci.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
